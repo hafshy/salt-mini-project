c7bb8a00cd2f3295476ad8b28d6abb15ff7d88b8
Hafshy <hafshy@gmail.com>
2023-10-20 16:29:34 +0700

    misc: add misc

---
+++ Pods/Target Support Files/Pods-SaltMiniProject-SaltMiniProjectUITests/Pods-SaltMiniProject-SaltMiniProjectUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SaltMiniProject_SaltMiniProjectUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SaltMiniProject_SaltMiniProjectUITests
+@end
---
+++ Pods/Target Support Files/Pods-SaltMiniProject/Pods-SaltMiniProject-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SaltMiniProject : NSObject
+@end
+@implementation PodsDummy_Pods_SaltMiniProject
+@end
